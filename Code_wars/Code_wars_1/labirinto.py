7a2599f24629b3236778c972539dccc1616bdf27
Lígia Pereira Silva de Oliveira <ligiapsoliveira@gmail.com>
2022-04-12 17:06:04 -0300

    Clean:Reorganized code of print and move function

--- Code_wars/Code_wars_1/labirinto.py
+++ Code_wars/Code_wars_1/labirinto.py
@@ -24,17 +24,19 @@ def input_user_maze(h:int=10,c:int=20)->list:
     return generate_maze(h,c)
 
 def print_maze(maze_map:list)->None:
-    """printa o labirinto e os numeros das colunas e linhas alinhados"""
-    line_list=[str(n) for n in range(l)]
-    l_size=max(map(len,line_list))#maximas casas decimais do numero de linhas do labirinto
-    column_numbers=[str(number)for number in range(c)]
-    c_size=max(map(len,column_numbers))#maximas casas decimais do numero de colunas do labirinto
+    """printa o labirinto com os numeros das colunas e linhas alinhados"""
+    l_size=len(str(l-1))#maximas casas decimais do numero de linhas do labirinto
+    c_size=len(str(c-1))#maximas casas decimais do numero de colunas do labirinto
+    
+    #padronizando numeros com mesmas casas decimais
+    line_list=map(lambda n:str(n).zfill(l_size),range(l))
+    column_numbers=map(lambda n:str(n).zfill(c_size),range(c))
+
     spacing=" "*c_size #espacamento necessario entre as colunas
-    print(" "*(l_size+1)+" ".join([number.zfill(c_size) for number in column_numbers]))
-
+   
+    print(" "*(l_size+1) + " ".join(column_numbers))
     for i in line_list:
-        print(f"{i.zfill(l_size)}{spacing}{spacing.join(maze_map[int(i)])}")   
-
+        print(i + spacing + spacing.join(maze_map[int(i)]))   
     return None
 
 def user_input(maze_map:list)->tuple:#pega a posicao inicial do usuario e verifica se e valido ex:"1,2"
@@ -58,15 +60,14 @@ def verify_movement(position:tuple,direction:list)->tuple:
 
 def move_robot(position:tuple,)->None:
     """movimenta o robo ate a saída"""
-
     ESQUERDA = [0, -1]
     DIREITA  = [0, 1]
     CIMA     = [-1, 0]
     BAIXO    = [1, 0]  
-
     visited_positions=[]
+    
     while True:
-        sleep(0.7)#espera 0,7segundos para rodar o proximo movimento do robo
+        sleep(0.7)#espera 0,7 segundos para rodar o proximo movimento do robo
         maze_map[position[0]][position[1]]="."#marca a posicao atual como percorrida
         directions_dict=dict()
         for direction in [CIMA,DIREITA,BAIXO,ESQUERDA]:
@@ -75,13 +76,11 @@ def move_robot(position:tuple,)->None:
                 directions_dict[(i,j)]=maze_map[i][j]#adiciona aos caminhos possiveis de serem tomados pelo robo
 
         if "S" in directions_dict.values():
-            for location,value in directions_dict.items():
-                if value=="S":
-                    i,j=location
-                    maze_map[i][j]="X"
-                    print_maze(maze_map)
-                    print("========|Saída Encontrada|========")
-                    return None
+            i,j=list(directions_dict.keys())[list(directions_dict.values()).index("S")]
+            maze_map[i][j]="X"
+            print_maze(maze_map)
+            print("========|Saída Encontrada|========")
+            return None
 
         elif " " in directions_dict.values():
             chosen_direction=list(directions_dict.keys())[list(directions_dict.values()).index(" ")]
@@ -99,6 +98,7 @@ def move_robot(position:tuple,)->None:
         print_maze(maze_map)
 
 if __name__ == "__main__":
+
     maze_map=input_user_maze()
     l=len(maze_map)#numero de linhas
     c=len(maze_map[0])#numero de colunas
@@ -106,8 +106,3 @@ if __name__ == "__main__":
     maze_map,position=user_input(maze_map)#pega a posicao inicial do robo dada pelo usuario
     print_maze(maze_map)
     move_robot(position)
-
-    
-
-
-
